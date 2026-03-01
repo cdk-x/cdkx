93dd17b20cc028fd1db9e24945970a9bf749854f
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2026-03-01 10:01:51 +0100

    feat(testing): :sparkles: add and method to pick up resource entry for easy testing

--- packages/testing/src/index.ts
+++ packages/testing/src/index.ts
@@ -1,7 +1,1 @@
-export {
-  TestProvider,
-  SpyProvider,
-  CustomSynthesizerProvider,
-} from './lib/test-provider.js';
-export { SynthHelpers } from './lib/synth-helpers.js';
-export { TestResources } from './lib/test-resources.js';
+export * from './lib';
