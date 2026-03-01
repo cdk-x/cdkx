0968b93b3ee0807077da4955ff2c6a1883422bf0
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2026-03-01 18:40:06 +0100

    chore: remove unncesary files

--- packages/core/src/lib/codegen/index.ts
+++
@@ -1,5 +0,0 @@
-export * from './types.js';
-export * from './type-mapper.js';
-export * from './open-api-extractor.js';
-export * from './resource-code-generator.js';
-export * from './resource-spec-writer.js';
