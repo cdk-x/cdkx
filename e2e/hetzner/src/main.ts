ce26103140641d1ac65672395842a54d3a5cd256
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2026-03-20 19:50:17 +0100

    feat(e2e): add hetzner networking stack with network, subnet and route

--- e2e/hetzner/src/main.ts
+++ e2e/hetzner/src/main.ts
@@ -1,9 +1,31 @@
 import { App, Stack } from '@cdkx-io/core';
+import {
+  HtzNetwork,
+  HtzSubnet,
+  HtzRoute,
+  NetworkSubnetType,
+  NetworkZone,
+} from '@cdkx-io/hetzner';
 
 const app = new App();
-const stack = new Stack(app, 'MyStack');
+const networking = new Stack(app, 'Networking');
 
-// Add your resources here
-void stack;
+const network = new HtzNetwork(networking, 'Network', {
+  name: 'e2e-network',
+  ipRange: '10.0.0.0/16',
+});
+
+new HtzSubnet(networking, 'Subnet', {
+  networkId: network.attrNetworkId,
+  type: NetworkSubnetType.CLOUD,
+  networkZone: NetworkZone.EU_CENTRAL,
+  ipRange: '10.0.1.0/24',
+});
+
+new HtzRoute(networking, 'Route', {
+  networkId: network.attrNetworkId,
+  destination: '10.100.0.0/24',
+  gateway: '10.0.1.1',
+});
 
 app.synth();
