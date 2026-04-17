ddcc02166fe882719f6945596542f3142bfca84f
Antonio Márquez Pérez <antonio.marquez@jumptothecloud.tech>
2026-04-17 16:15:32 +0200

    feat(multipass): fix vm schema with readOnly attrs, remove servers resource

--- packages/providers/multipass/multipass/src/lib/provider/provider.ts
+++ packages/providers/multipass/multipass/src/lib/provider/provider.ts
@@ -3,9 +3,6 @@ import { Provider } from '@cdk-x/core';
 /**
  * Provider for Multipass resources.
  *
- * Use with a `Stack` and `YamlFileSynthesizer` to synthesize Multipass VM
- * configuration files committed to the repository.
- *
  * @example
  * ```ts
  * const provider = new MltProvider();
